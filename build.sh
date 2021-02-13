eaba77e3e16a08485cb6bd71b10a2499e7e37b7f
khas-flomesh <khas@flomesh.io>
2021-02-13 13:13:44 +0800

    Remove long option support of build.sh.

--- build.sh
+++ build.sh
@@ -25,7 +25,6 @@ SHORT_OPTS="bthr:"
 LONG_OPTS="test:,help,build-only,test-only"
 
 function usage() {
-  if [ "`uname`" == "Darwin" ] ; then
     echo "Usage: $0 [-h|-b|-t|-r <xxx>]" 1>&2
     echo "       -h           Show this help message"
     echo "       -b           Build only, do not run any test cases"
@@ -33,41 +32,27 @@ function usage() {
     echo "       -r <number>  Run specific test case, with number, like 001"
     echo ""
     exit 1
-  else
-    echo "Usage: $0 [-h|--help] [-b|--build-only] [-t|--test-only] [-r|--test <xxx>]" 1>&2
-    echo "       -h/--help: Show this help message"
-    echo "       -b/--build-only: Build only, do not run any test cases"
-    echo "       -t/--test-only: Test only, do not build pipy binary"
-    echo "       -r/--test <number>: Run specific test case, with number, like 001"
-    echo ""
-    exit 1
-  fi
 }
 
-if [ "`uname`" == "Darwin" ] ; then
-  OPTS=$(getopt $SHORT_OPTS "$@")
-else
-  OPTS=$(getopt --options $SHORT_OPTS --long $LONG_OPTS --name "$(basename "$0")" -- "$@")
-fi
-
+OPTS=$(getopt $SHORT_OPTS "$@")
 if [ $? != 0 ] ; then echo "Failed to parse options...exiting." >&2 ; exit 1 ; fi
 
 eval set -- "$OPTS"
 while true ; do
   case "$1" in
-    -b | --build-only)
+    -b)
       BUILD_ONLY=true
       shift
       ;;
-    -t | --test-only)
+    -t)
       TEST_ONLY=true
       shift
       ;;
-    -r | --test)
+    -r)
       TEST_CASE+="$2 "
       shift 2
       ;;
-    -h | --help)
+    -h)
         usage
         ;;
     --)
@@ -106,9 +91,10 @@ function build() {
   export CC=clang
   export CXX=clang++
   mkdir ${PIPY_DIR}/build 2>&1 > /dev/null || true
+  rm -fr ${PIPY_DIR}/build/*
   cd ${PIPY_DIR}/build
-  $CMAKE -DCMAKE_BUILD_TYPE=Release $PIPY_DIR 2>&1 > /dev/null
-  make -j${__NPROC} 2>&1 > /dev/null
+  $CMAKE -DCMAKE_BUILD_TYPE=Release $PIPY_DIR
+  make -j${__NPROC}
   cd - 2>&1 > /dev/null
   echo "pipy now is in ${PIPY_DIR}/bin"
 }
