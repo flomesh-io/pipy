e7989d7bcc731ca9e09986f7d3fe27ab90603e0e
pajama-coder <pajamacoder@flomesh.io>
2021-06-15 15:41:05 +0800

    Added test/010-jwt and test/016-socks4-proxy

---
+++ test/016-socks4-proxy/pipy.js
@@ -0,0 +1,19 @@
+pipy({
+  _addr: '',
+  _port: 0,
+})
+
+.listen(6080)
+  .proxySOCKS4(
+    'connect',
+    (addr, port) => (
+      _addr = addr,
+      _port = port,
+      true // return true to accept the connection
+    )
+  )
+
+.pipeline('connect')
+  .connect(
+    () => `${_addr}:${_port}`
+  )
