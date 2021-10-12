cd7ca9605077c0d95b2d8a6bdebebd161d9095d3
pajama-coder <pajamacoder@flomesh.io>
2021-10-12 13:36:02 +0800

    [fix] Non-local-file-system codebases could not find a file path without a slash prefix

--- src/codebase.cpp
+++ src/codebase.cpp
@@ -179,17 +179,9 @@ public:
   virtual bool writable() const override { return false; }
   virtual auto entry() const -> const std::string& override { return m_entry; }
   virtual void entry(const std::string &path) override {}
-
   virtual auto list(const std::string &path) -> std::list<std::string> override;
-
-  virtual auto get(const std::string &path) -> pipy::Data* override {
-    auto i = m_files.find(path);
-    if (i == m_files.end()) return nullptr;
-    return i->second;
-  }
-
+  virtual auto get(const std::string &path) -> pipy::Data* override;
   virtual void set(const std::string &path, Data *data) override {}
-
   virtual void sync(const Status &status, const std::function<void(bool)> &on_update) override {}
 
 private:
@@ -241,6 +233,14 @@ auto CodebsaeFromStore::list(const std::string &path) -> std::list<std::string> 
   return list;
 }
 
+auto CodebsaeFromStore::get(const std::string &path) -> pipy::Data* {
+  auto k = path;
+  if (k.front() != '/') k.insert(k.begin(), '/');
+  auto i = m_files.find(k);
+  if (i == m_files.end()) return nullptr;
+  return i->second;
+}
+
 //
 // CodebaseFromHTTP
 //
@@ -255,17 +255,9 @@ private:
   virtual bool writable() const override { return false; }
   virtual auto entry() const -> const std::string& override { return m_entry; }
   virtual void entry(const std::string &path) override {}
-
   virtual auto list(const std::string &path) -> std::list<std::string> override;
-
-  virtual auto get(const std::string &path) -> pipy::Data* override {
-    auto i = m_files.find(path);
-    if (i == m_files.end()) return nullptr;
-    return i->second;
-  }
-
+  virtual auto get(const std::string &path) -> pipy::Data* override;
   virtual void set(const std::string &path, Data *data) override {}
-
   virtual void sync(const Status &status, const std::function<void(bool)> &on_update) override;
 
   pjs::Ref<URL> m_url;
@@ -325,6 +317,14 @@ auto CodebaseFromHTTP::list(const std::string &path) -> std::list<std::string> {
   std::list<std::string> list;
   for (const auto &name : names) list.push_back(name);
   return list;
+}
+
+auto CodebaseFromHTTP::get(const std::string &path) -> pipy::Data* {
+  auto k = path;
+  if (k.front() != '/') k.insert(k.begin(), '/');
+  auto i = m_files.find(k);
+  if (i == m_files.end()) return nullptr;
+  return i->second;
 }
 
 void CodebaseFromHTTP::sync(const Status &status, const std::function<void(bool)> &on_update) {
