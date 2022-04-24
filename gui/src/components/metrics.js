53f699e68850c6a7ec58b9ef88bb08dd1add0a64
pajama-coder <pajamacoder@flomesh.io>
2022-04-24 21:43:36 +0800

    [core] Draw sparkline charts for sub-metrics

--- gui/src/components/metrics.js
+++ gui/src/components/metrics.js
@@ -53,7 +53,6 @@ const useStyles = makeStyles(theme => ({
   },
   sparkline: {
     width: '100%',
-    height: '50px',
   },
   pointNumber: {
     position: 'relative',
@@ -63,6 +62,20 @@ const useStyles = makeStyles(theme => ({
     fontSize: '20px',
     fontWeight: 'bolder',
     pointerEvents: 'none',
+  },
+  chart: {
+    width: '100%',
+  },
+  chartRow: {
+    backgroundColor: '#1c1c1c',
+  },
+  chartRowTitle: {
+  },
+  chartRowNumber: {
+    width: '100px',
+  },
+  chartRowSparkline: {
+    width: '50%',
   },
 }));
 
@@ -129,9 +142,15 @@ function Metrics({ root }) {
               ) : (
                 <List dense disablePadding>
                   {metricList.map(({ k: name, v: values }) => (
-                    <ListItem key={name} disableGutters button>
+                    <ListItem
+                      key={name}
+                      selected={currentMetric === name}
+                      disableGutters
+                      button
+                      onClick={() => setCurrentMetric(name)}
+                    >
                       <div className={classes.listItem}>
-                        <Sparkline title={name} values={values}/>
+                        <MetricItem title={name} values={values}/>
                       </div>
                     </ListItem>
                   ))}
@@ -143,7 +162,11 @@ function Metrics({ root }) {
 
           {/* Metric Chart */}
           <Pane initialSize={splitPos[0][1]} className={classes.metricChartPane}>
-            <Nothing text="No metric selected"/>
+            {currentMetric ? (
+              <Chart path={root} uuid={instance?.id ? uuid : ''} title={currentMetric}/>
+            ) : (
+              <Nothing text="No metric selected"/>
+            )}
           </Pane>
 
         </SplitPane>
@@ -152,113 +175,266 @@ function Metrics({ root }) {
   );
 }
 
-function Sparkline({ title, values }) {
+function MetricItem({ title, values }) {
   const classes = useStyles();
-  const canvasEl = React.useRef(null);
-  const numberEl = React.useRef(null);
-  const cursorEl = React.useRef(null);
-  const circleEl = React.useRef(null);
+  const [cursorX, setCursorX] = React.useState(-1);
 
-  const resetNumber = () => {
-    const n = values.length;
-    const v = values[n - 1] || 0;
-    numberEl.current.innerText = v;
-    cursorEl.current.setAttribute('stroke', 'none');
-    circleEl.current.setAttribute('stroke', 'none');
+  const handleCursorMove = (x) => {
+    setCursorX(x);
   }
 
-  React.useEffect(
-    resetNumber,
-    [values, numberEl]
+  const { min, max } = React.useMemo(
+    () => {
+      let min = Number.POSITIVE_INFINITY;
+      let max = Number.NEGATIVE_INFINITY;
+    
+      values.forEach(
+        v => {
+          const y = -(v || 0);
+          if (y < min) min = y;
+          if (y > max) max = y;  
+        }
+      );
+    
+      if (max < 0) max = 0;
+      if (max - min < 100) min = max - 100;
+
+      return { min, max };
+    },
+    [values]
   );
 
-  const line = [];
-  let min = Number.POSITIVE_INFINITY;
-  let max = Number.NEGATIVE_INFINITY;
+  const i = 60 - cursorX;
+  const n = values.length;
+  const y = cursorX < 0 ? values[n-1] : (values[n-i] || 0);
 
-  for (let i = 1; i <= 60; i++) {
-    const x = 60 - i;
-    const y = -(values[values.length - i] || 0);
-    if (y < min) min = y;
-    if (y > max) max = y;
-    line.push(i > 1 ? `L ${x},${y}` : `M ${x},${y}`);
+  return (
+    <div>
+      <Typography color="textSecondary">{title}</Typography>
+      <Sparkline
+        values={values}
+        min={min}
+        max={max}
+        height={50}
+        cursorX={cursorX}
+        onCursorMove={handleCursorMove}
+      />
+      <div className={classes.pointNumber}>
+        {y}
+      </div>
+    </div>
+  );
+}
+
+function Chart({ path, uuid, title }) {
+  const classes = useStyles();
+  const [cursorX, setCursorX] = React.useState(-1);
+
+  const queryMetric = useQuery(
+    `metrics:${path}:${uuid}:${title}`,
+    async () => {
+      if (uuid) {
+        const res = await fetch(`/api/v1/metrics/${uuid}/${title}`);
+        if (res.status === 200) {
+          const data = await res.json();
+          return data.metrics;
+        }
+      } else {
+        const res = await fetch(`/api/v1/metrics/${title}`);
+        if (res.status === 200) {
+          const data = await res.json();
+          return data.metrics;
+        }
+      }
+      return null;
+    },
+    {
+      refetchInterval: 1000,
+    }
+  );
+
+  const { list, min, max } = React.useMemo(
+    () => {
+      const root = queryMetric.data?.[0];
+      const list = [];
+
+      const traverse = (k, m) => {
+        if (m.s instanceof Array) {
+          if (k.length > 0) k += '/';
+          m.s.forEach(
+            m => traverse(k + m.k, m)
+          );
+        } else {
+          list.push({ key: k, values: m.v });
+        }
+      }
+
+      if (root) {
+        traverse('', root);
+      }
+
+      let min = Number.POSITIVE_INFINITY;
+      let max = Number.NEGATIVE_INFINITY;
+
+      list.forEach(
+        ({values}) => (
+          values.forEach(
+            v => {
+              const y = -(v || 0);
+              if (y < min) min = y;
+              if (y > max) max = y;
+            }
+          )
+        )
+      );
+
+      if (max < 0) max = 0;
+      if (max - min < 100) min = max - 100;
+
+      return { list, min, max };
+    },
+    [queryMetric.data]
+  );
+
+  const handleCursorMove = (x) => {
+    setCursorX(x);
   }
 
-  if (max < 0) max = 0;
-  if (max - min < 100) min = max - 100;
+  return (
+    <table className={classes.chart}>
+      {list.map(
+        ({ key, values }) => (
+          <ChartRow
+            title={key}
+            values={values}
+            min={min}
+            max={max}
+            cursorX={cursorX}
+            onCursorMove={handleCursorMove}
+          />
+        )
+      )}
+    </table>
+  );
+}
+
+function ChartRow({ title, values, min, max, cursorX, onCursorMove }) {
+  const classes = useStyles();
+  const i = 60 - cursorX;
+  const n = values.length;
+  const y = cursorX < 0 ? values[n-1] : (values[n-i] || 0);
+  return (
+    <tr className={classes.chartRow}>
+      <td className={classes.chartRowTitle}>
+        <Typography>{title}</Typography>
+      </td>
+      <td className={classes.chartRowNumber}>
+        <Typography>{y}</Typography>
+      </td>
+      <td className={classes.chartRowSparkline}>
+        <Sparkline
+          values={values}
+          min={min}
+          max={max}
+          height={26}
+          cursorX={cursorX}
+          onCursorMove={onCursorMove}
+        />
+      </td>
+    </tr>
+  );
+}
+
+let counter = 0;
+
+function Sparkline({ values, min, max, height, cursorX, onCursorMove }) {
+  const classes = useStyles();
+  const canvasEl = React.useRef(null);
+  const cursorEl = React.useRef(null);
+  const circleEl = React.useRef(null);
 
   const margin = (max - min) * 0.1;
   min -= margin;
   max += margin;
 
+  const { edge, area } = React.useMemo(
+    () => {
+      console.log(counter++);
+
+      const line = [];
+      for (let i = 1; i <= 60; i++) {
+        const x = 60 - i;
+        const y = -(values[values.length - i] || 0);
+        line.push(i > 1 ? `L ${x},${y}` : `M ${x},${y}`);
+      }
+      const edge = line.join(' ');
+      const area = edge + `L 0,${max-margin} L 59,${max-margin} z`;
+      return { edge, area };
+    },
+    [values, min, max]
+  );
+  
   const handleMouseMove = e => {
     const rect = canvasEl.current.getBoundingClientRect();
     const d = rect.x + rect.width - e.pageX;
     const i = Math.max(0, Math.min(59, (d * 59 / rect.width) | 0));
     const x = 59 - i;
-    const n = values.length;
-    const v = values[n - 1 - i] || 0;
-    const y = -v;
-    const cursor = cursorEl.current;
-    const circle = circleEl.current;
-    cursor.setAttribute('stroke', 'green');
-    cursor.setAttribute('x1', x);
-    cursor.setAttribute('x2', x);
-    circle.setAttribute('stroke', 'green');
-    circle.setAttribute('x1', x);
-    circle.setAttribute('x2', x);
-    circle.setAttribute('y1', y);
-    circle.setAttribute('y2', y);
-    numberEl.current.innerText = v;
+    onCursorMove(x);
   }
 
-  const edge = line.join(' ');
-  const area = edge + `L 0,${max-margin} L 59,${max-margin} z`;
+  const x = cursorX;
+  const i = 59 - x;
+  const n = values.length;
+  const v = values[n - 1 - i] || 0;
+  const y = -v;
 
   return (
-    <div>
-      <Typography color="textSecondary">{title}</Typography>
-      <svg
-        ref={canvasEl}
-        viewBox={`0 ${min} 60 ${max - min}`}
-        preserveAspectRatio="none"
-        className={classes.sparkline}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={resetNumber}
-      >
-        <path
-          d={area}
-          fill="green"
-          fillOpacity="25%"
-          stroke="none"
-        />
-        <path
-          d={edge}
-          vectorEffect="non-scaling-stroke"
-          fill="none"
-          stroke="green"
-          strokeWidth="2"
-          strokeLinejoin="round"
-        />
-        <line
-          ref={cursorEl}
-          y1={min}
-          y2={max}
-          vectorEffect="non-scaling-stroke"
-          stroke="none"
-          strokeWidth="1"
-          strokeLinejoin="round"
-        />
-        <line
-          ref={circleEl}
-          vectorEffect="non-scaling-stroke"
-          stroke="none"
-          strokeWidth="8"
-          strokeLinecap="round"
-        />
-      </svg>
-      <div ref={numberEl} className={classes.pointNumber}/>
-    </div>
+    <svg
+      ref={canvasEl}
+      viewBox={`0 ${min} 60 ${max - min}`}
+      preserveAspectRatio="none"
+      className={classes.sparkline}
+      style={{ height }}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={() => onCursorMove(-1)}
+    >
+      <path
+        d={area}
+        fill="green"
+        fillOpacity="25%"
+        stroke="none"
+      />
+      <path
+        d={edge}
+        vectorEffect="non-scaling-stroke"
+        fill="none"
+        stroke="green"
+        strokeWidth="2"
+        strokeLinejoin="round"
+      />
+      <line
+        ref={cursorEl}
+        x1={x}
+        x2={x}
+        y1={min}
+        y2={max}
+        vectorEffect="non-scaling-stroke"
+        stroke={cursorX >= 0 ? 'green' : 'none'}
+        strokeWidth="1"
+        strokeLinejoin="round"
+      />
+      <line
+        ref={circleEl}
+        vectorEffect="non-scaling-stroke"
+        x1={x}
+        y1={y}
+        x2={x}
+        y2={y}
+        stroke={cursorX >= 0 ? 'white' : 'none'}
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+    </svg>
   );
 }
 
