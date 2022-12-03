04257ee94d3d83ca357e60650138561ff3670240
pajama-coder <pajamacoder@flomesh.io>
2022-12-03 21:22:43 +0800

    [sample] Add retry to load-balancing module in the sidecar sample

--- samples/sidecar/modules/outbound-http-load-balancing.js
+++ samples/sidecar/modules/outbound-http-load-balancing.js
@@ -5,53 +5,101 @@
   tlsPrivateKey = config?.Certificate?.PrivateKey,
   tlsIssuingCA = config?.Certificate?.IssuingCA,
 
-  makeTargetBalancer = (clusterName) => (
+  makeClusterConfig = (clusterName) => (
     (
-      targets = config.Outbound.ClustersConfigs[clusterName].Endpoints
+      clusterConfig = config?.Outbound?.ClustersConfigs?.[clusterName],
     ) => (
-      new algo.RoundRobinLoadBalancer(targets)
+      {
+        targetBalancer: new algo.RoundRobinLoadBalancer(clusterConfig?.Endpoints),
+        needRetry: Boolean(clusterConfig?.RetryPolicy?.NumRetries),
+        numRetries: clusterConfig?.RetryPolicy?.NumRetries,
+        lowerbound: clusterConfig?.RetryPolicy?.RetryOn ? clusterConfig.RetryPolicy.RetryOn.replaceAll('x', '0') : 500,
+        upperbound: clusterConfig?.RetryPolicy?.RetryOn ? clusterConfig.RetryPolicy.RetryOn.replaceAll('x', '9') : 599,
+        retryBackoffBaseInterval: clusterConfig?.RetryPolicy?.RetryBackoffBaseInterval || 1, // default 1 second
+        muxHttpOptions: {
+          version: () => __isHTTP2 ? 2 : 1,
+          maxMessages: clusterConfig?.ConnectionSettings?.http?.MaxRequestsPerConnection
+        }
+      }
     )
   )(),
 
-  targetBalancers = new algo.Cache(makeTargetBalancer),
+  clusterConfigs = new algo.Cache(makeClusterConfig),
 
   makeClusterBalancer = (clusters) => (
     (
       balancer = new algo.RoundRobinLoadBalancer(clusters)
     ) => (
-      () => targetBalancers.get(balancer.next()?.id).next()
+      () => (
+        clusterConfigs.get(balancer.next()?.id)
+      )
     )
   )(),
 
   clusterBalancers = new algo.Cache(makeClusterBalancer),
 
+  calcScaleRatio = (n) => (
+    n < 1 ? 0 : (n = Math.pow(2, n - 1), n > 10 ? 10 : n)
+  ),
+
+  shouldRetry = (statusCode) => (
+    (_clusterConfig.lowerbound <= _statusCode && statusCode <= _clusterConfig.upperbound) &&
+    (_retryCount++ < _clusterConfig.numRetries)
+  ),
+
 ) => pipy({
   _target: null,
+  _retryCount: null,
+  _clusterConfig: null,
 })
 
 .import({
+  __isHTTP2: 'outbound-main',
   __route: 'outbound-http-routing',
 })
 
 .pipeline()
+.onStart(
+  () => void (
+    (_clusterConfig = clusterBalancers.get(__route?.TargetClusters)()) && (
+      _target = _clusterConfig.targetBalancer?.next?.()
+    )
+  )
+)
 .branch(
-  () => (_target = clusterBalancers.get(__route?.TargetClusters)()), (
+  () => !_target, $=>$.chain(),
+  () => _clusterConfig.needRetry, (
     $=>$
-    .muxHTTP(() => _target).to(
-      $=>$
-      .connectTLS({
-        certificate: () => ({
-          cert: new crypto.Certificate(tlsCertChain),
-          key: new crypto.PrivateKey(tlsPrivateKey),
-        }),
-        trusted: tlsIssuingCA ? [new crypto.Certificate(tlsIssuingCA)] : [],
-      }).to(
-        $=>$.connect(() => _target.id)
+    .handleMessageStart(
+      () => (
+        _retryCount = 0
       )
     )
-
+    .replay({ 'delay': () => _clusterConfig.retryBackoffBaseInterval * calcScaleRatio(_retryCount) }).to(
+      $=>$
+      .link('upstream')
+      .replaceMessageStart(
+        evt => (
+          shouldRetry(evt.head.status) ? new StreamEnd('Replay') : evt
+        )
+      )
+    )
   ), (
-    $=>$.chain()
+    $=>$.link('upstream')
+  )
+)
+
+.pipeline('upstream')
+.muxHTTP(() => _target, () => _clusterConfig.muxHttpOptions).to(
+  $=>$
+  .connectTLS({
+    certificate: () => ({
+      cert: new crypto.Certificate(tlsCertChain),
+      key: new crypto.PrivateKey(tlsPrivateKey),
+    }),
+    trusted: tlsIssuingCA ? [new crypto.Certificate(tlsIssuingCA)] : [],
+  }).to(
+    $=>$.connect(() => _target.id)
   )
 )
 
