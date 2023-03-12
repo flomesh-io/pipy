f76321a99b643b9f1f4456f5dac201b771231ce7
pajama-coder <pajamacoder@flomesh.io>
2023-03-12 20:05:50 +0800

    [test] Add test for Hessian codec

--- test/codec/dubbo/main.js
+++ test/codec/dubbo/main.js
@@ -1,8 +1,15 @@
 pipy()
 
 .task()
-.onStart(() => new Message)
+.onStart(new Message)
 .read('input')
 .decodeDubbo()
+.replaceMessageBody(
+  data => (
+    Hessian.encode(
+      Hessian.decode(data)
+    )
+  )
+)
 .encodeDubbo()
 .tee('-')
