e123cf7562b02dbbc413b48f0e01ff16809b48ee
pajama-coder <pajamacoder@flomesh.io>
2023-04-22 14:52:37 +0800

    [sample] Rewrite samples/serve

--- samples/serve/config.js
+++
@@ -1,1 +0,0 @@
-JSON.decode(pipy.load('config.json'))
--- samples/serve/main.js
+++ samples/serve/main.js
@@ -1,34 +1,71 @@
 ((
-  config = pipy.solve('config.js'),
-
+  config = JSON.decode(pipy.load('config.json')),
+  response404 = new Message({ status: 404 }, 'Not found'),
 ) =>
 
 pipy({
-  _method: '',
+  _service: undefined,
+  _target: undefined,
 })
 
-  .listen(config.port || 80)
-  .demuxHTTP().to(
-    $=>$
-    .handleMessageStart(
-      msg => _method = msg.head.method
-    )
-    .branch(
-      () => _method === 'GET' || _method === 'HEAD', (
-        $=>$
-        .chain([
-          'plugins/cache.js',
-          'plugins/serve.js',
-          'plugins/default.js',
-        ])
-      ), (
-        $=>$
-        .replaceData()
-        .replaceMessage(
-          new Message({ status: 405 }, 'Method Not Allowed')
+.repeat(config.http, (
+  ($, { listen, services, routes }, i) => (
+    (
+      router = new algo.URLRouter(routes),
+    ) => (
+      Object.values(services).forEach(
+        s => select(s.type,
+          'proxy', () => Object.assign(s, {
+            balancer: new algo.RoundRobinLoadBalancer(s.targets),
+            rewrite: s.rewrite && (
+              (
+                pat = new RegExp(s.rewrite.pattern),
+                rep = s.rewrite.replace,
+              ) => (
+                head => head.path = head.path.replace(pat, rep)
+              )
+            )(),
+          }),
+          'files', () => s.files = Object.fromEntries(
+            pipy.list(s.path).map(k => [`/${k}`, http.File.from(`${s.path}/${k}`)]).concat([
+              ['/', http.File.from(`${s.path}/index.html`)]
+            ])
+          )
         )
-      )
+      ),
+      $
+      .repeat(listen.tcp.ports, ($, p) => $.listen(p).link(`http-tcp-${i}`))
+      .repeat(listen.tls.ports, ($, p) => $.listen(p).link(`http-tls-${i}`))
+      .pipeline(`http-tls-${i}`)
+        .acceptTLS({
+          certificate: {
+            cert: new crypto.Certificate(pipy.load(listen.tls.certificates.cert)),
+            key: new crypto.PrivateKey(pipy.load(listen.tls.certificates.key)),
+          }
+        }).to(`http-tcp-${i}`)
+      .pipeline(`http-tcp-${i}`)
+        .demuxHTTP().to(
+          $=>$
+          .branchMessageStart(
+            ({ head }) => (
+              _service = services[router.find(head.headers.host, head.path)],
+              _service?.type === 'files'
+            ), (
+              $=>$.replaceMessage(
+                ({ head }) => _service.files[head.path]?.toMessage?.(head.headers['accept-encoding']) || response404
+              )
+            ),
+            ({ head }) => _service?.type === 'proxy' && !void(_service.rewrite?.(head)), (
+              $=>$.muxHTTP(() => _target = _service.balancer.borrow()).to(
+                $=>$.connect(() => _target.id)
+              )
+            ), (
+              $=>$.replaceMessage(response404)
+            )
+          )
+        )
     )
-  )
+  )()
+))
 
 )()
