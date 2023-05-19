9fee072ec78570d768306f836a6dfcdae5d43e42
pajama-coder <pajamacoder@flomesh.io>
2023-05-19 16:12:54 +0800

    [test] Add HTTP/2 tunnel test

---
+++ test/curl/005-http2-tunnel/main.js
@@ -0,0 +1,31 @@
+pipy({
+  _target: undefined,
+})
+
+.listen(8080)
+.serveHTTP(new Message('Hello!\n'))
+
+.listen(8000)
+.demuxHTTP().to($=>$
+  .acceptHTTPTunnel(
+    msg => msg.head.method === 'CONNECT' ? (
+      void (_target = msg.head.path)
+    ) : (
+      new Message({ status: 405 }, 'method not allowed\n')
+    )
+  ).to($=>$
+    .connect(() => _target)
+  )
+)
+
+.listen(9000)
+.connectHTTPTunnel(
+  () => new Message({
+    method: 'CONNECT',
+    path: 'localhost:8080',
+  })
+).to($=>$
+  .muxHTTP(() => 1, { version: 2 }).to($=>$
+    .connect('localhost:8000')
+  )
+)
