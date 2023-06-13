1bc5add118df52195d1fd671e8ca47e69626bf88
pajama-coder <pajamacoder@flomesh.io>
2023-06-13 20:46:37 +0800

    [sample] Minimize makefile for the packet-counter BPF sample

--- samples/bpf/packet-counter/main.js
+++ samples/bpf/packet-counter/main.js
@@ -1,4 +1,31 @@
-pipy()
+((
+  map = bpf.Map.open(
+    bpf.Map.list().find(
+      i => i.name === 'pkt_cnt_stats'
+    )?.id,
+    new CStruct().field('uint32', 'ip'),
+    new CStruct().field('uint32', 'count'),
+  ),
+
+) => pipy()
 
 .listen(8080)
 .serveHTTP(new Message('hello'))
+
+.task()
+.onStart(
+  () => (
+    map.update({ ip: 0x0100007f }, { count: 0 }),
+    new StreamEnd
+  )
+)
+
+.task('5s')
+.onStart(
+  () => (
+    console.log(map.lookup({ ip: 0x0100007f })),
+    new StreamEnd
+  )
+)
+
+)()
