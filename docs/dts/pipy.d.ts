ea99e48b6e0dd856b10a0184160b53d4c727d1bc
pajama-coder <pajamacoder@flomesh.io>
2023-10-27 09:44:06 +0800

    [doc] Update DTS file for functions under the pipy namespace

--- docs/dts/pipy.d.ts
+++ docs/dts/pipy.d.ts
@@ -8,7 +8,12 @@
  */
 declare function pipy(contextPrototype? : { [name: string]: any }) : Configuration;
 
-declare namespace pipy {
+declare interface pipy {
+
+  /**
+   *
+   */
+  pid: number;
 
   /**
    * Reads content of a file from the current codebase.
@@ -16,7 +21,7 @@ declare namespace pipy {
    * @param filename A pathname in the current codebase.
    * @returns A _Data_ object containing the entire content of the specified file.
    */
-  function load(filename: string): Data;
+  load(filename: string): Data;
 
   /**
    * Evaluates expression from a file in the current codebase.
@@ -24,17 +29,17 @@ declare namespace pipy {
    * @param filename A pathname in the current codebase.
    * @returns A value of any type after evaluating the expression in the specified file.
    */
-  function solve(filename: string): any;
+  solve(filename: string): any;
 
   /**
    * Reloads the current codebase.
    */
-  function restart(): void;
+  restart(): void;
 
   /**
    * Gracefully shuts down Pipy.
    *
    * @param exitCode A number for the process exit code.
    */
-  function exit(exitCode?: number): void;
+  exit(exitCode?: number): void;
 }
