0c655bd49e4012edc83ef3a91ae744daeba75227
pajama-coder <pajamacoder@flomesh.io>
2024-02-21 10:58:20 +0800

    [test] Altered benchmark/003-http-mux to using the connection pool of the new LoadBalancer API

--- test/benchmark/003-http-mux/main.js
+++ test/benchmark/003-http-mux/main.js
@@ -1,8 +1,12 @@
-pipy()
+var lb = new algo.LoadBalancer([ 'localhost:8080' ])
 
-.listen(os.env.LISTEN || 8000)
-.demuxHTTP().to($=>$
-  .muxHTTP().to($=>$
-    .connect('localhost:8080')
+var $conn
+
+pipy.listen(os.env.LISTEN || 8000, $=>$
+  .demuxHTTP().to($=>$
+    .muxHTTP(() => $conn = lb.allocate()).to($=>$
+      .connect(() => $conn.target)
+    )
+    .onEnd(() => $conn.free())
   )
 )
