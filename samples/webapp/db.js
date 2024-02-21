5b75a51e821b0032481c2eacde426f5e20711ac8
pajama-coder <pajamacoder@flomesh.io>
2024-02-21 13:27:58 +0800

    [sample] Rewrite sample/webapp in the new style of pjs

--- samples/webapp/db.js
+++ samples/webapp/db.js
@@ -1,72 +1,51 @@
-((
-  db = sqlite('data.db')
-) => (
-  db.exec(
-    `SELECT * FROM sqlite_schema WHERE type = 'table' AND name = 'todos'`
-  ).length === 0 && (
-    db.exec(`
-      CREATE TABLE todos (
-        id INTEGER PRIMARY KEY,
-        content TEXT NOT NULL,
-        checked INTEGER NOT NULL DEFAULT 0
-      )
-    `)
-  ),
+var db = sqlite('data.db')
 
-  {
-    listTodos: () => (
-      db.sql(
-        `SELECT * FROM todos`
-      )
-      .exec()
-    ),
+if (db.exec(
+  `SELECT * FROM sqlite_schema WHERE type = 'table' AND name = 'todos'`
+).length === 0) {
+  db.exec(`
+    CREATE TABLE todos (
+      id INTEGER PRIMARY KEY,
+      content TEXT NOT NULL,
+      checked INTEGER NOT NULL DEFAULT 0
+    )
+  `)
+}
 
-    getTodo: (id) => (
-      db.sql(
-        `SELECT * FROM todos WHERE id = ${id}`
-      )
+export function listTodos() {
+  return db.sql(`SELECT * FROM todos`).exec()
+}
+
+export function getTodo(id) {
+  return db.sql(`SELECT * FROM todos WHERE id = ${id}`).exec()[0]
+}
+
+export function insertTodo(content) {
+  db.sql(`INSERT INTO todos (content) VALUES (?)`)
+    .bind(1, content)
+    .exec()
+  return (
+    db.sql(`SELECT * FROM todos WHERE id = last_insert_rowid()`)
       .exec()[0]
-    ),
+  )
+}
 
-    insertTodo: (content) => (
-      db.sql(
-        `INSERT INTO todos (content) VALUES (?)`
-      )
-      .bind(1, content)
-      .exec(),
-      db.sql(
-        `SELECT * FROM todos WHERE id = last_insert_rowid()`
-      )
+export function updateTodo(id, content) {
+  db.sql(`UPDATE todos SET content = ? WHERE id = ?`)
+    .bind(1, content)
+    .bind(2, id)
+    .exec()
+  return (
+    db.sql(`SELECT * FROM todos WHERE id = ${id}`)
       .exec()[0]
-    ),
+  )
+}
 
-    updateTodo: (id, content) => (
-      db.sql(
-        `UPDATE todos SET content = ? WHERE id = ?`
-      )
-      .bind(1, content)
-      .bind(2, id)
-      .exec(),
-      db.sql(
-        `SELECT * FROM todos WHERE id = ${id}`
-      )
-      .exec()[0]
-    ),
+export function checkTodo(id, checked) {
+  db.exec(`UPDATE todos SET checked = ${checked} WHERE id = ${id}`)
+  return db.sql(`SELECT * FROM todos WHERE id = ${id}`).exec()[0]
+}
 
-    checkTodo: (id, checked) => (
-      db.exec(
-        `UPDATE todos SET checked = ${checked} WHERE id = ${id}`
-      ),
-      db.sql(
-        `SELECT * FROM todos WHERE id = ${id}`
-      )
-      .exec()[0]
-    ),
-
-    deleteTodo: (id) => void (
-      db.exec(
-        `DELETE FROM todos WHERE id = ${id}`
-      )
-    ),
-  }
-))()
+export function deleteTodo(id) {
+  return db.exec(`DELETE FROM todos WHERE id = ${id}`)
+}
--- samples/webapp/main.js
+++ samples/webapp/main.js
@@ -1,86 +1,50 @@
-pipy()
+import * as db from './db.js'
 
-.listen(8080)
-.demuxHTTP().to($=>$
-  .branchMessageStart(
+pipy.listen(8080, $=>$.serveHTTP(
+  function (req) {
+    var path = req.head.path
+    if (path.startsWith('/api/')) {
+      return api.find(path)(req)
+    } else {
+      return www.serve(req) || page404
+    }
+  }
+))
 
-    // API endpoints
-    msg => msg.head.path.startsWith('/api/'), (
-      (
-        db = pipy.solve('db.js'),
+var page404 = new Message({ status: 404 })
 
-        response = (status, obj) => (
-          new Message(
-            {
-              status,
-              headers: {
-                'content-type': 'application/json'
-              }
-            },
-            status === 204 ? undefined : JSON.encode(obj || { status })
-          )
-        ),
+var www = new http.Directory('www/public')
 
-        router = new algo.URLRouter({
-          '/api/todos': req => (
-            select(req.head.method,
-              'GET', () => (
-                response(200, db.listTodos())
-              ),
-              'POST', () => (
-                response(201, db.insertTodo(req.body.toString()))
-              ), (
-                response(405)
-              )
-            )
-          ),
-
-          '/api/todos/*': req => (
-            (
-              id = req.head.path.substring(11)|0,
-            ) => (
-              select(req.head.method,
-                'GET', () => (
-                  response(200, db.getTodo(id))
-                ),
-                'PATCH', () => (
-                  response(200, db.updateTodo(id, req.body.toString()))
-                ),
-                'DELETE', () => (
-                  response(204, db.deleteTodo(id))
-                ), (
-                  response(405)
-                )
-              )
-            )
-          )(),
-
-          '/api/check-todo/*': req => (
-            response(200, db.checkTodo(
-              req.head.path.substring(16)|0,
-              req.body.toString() === 'true',
-            ))
-          ),
-
-          '/*': () => response(404),
-        }),
-
-      ) => ($=>$
-        .replaceMessage(
-          req => router.find(req.head.path)(req)
-        )
-      )
-    )(),
-
-    // Static files
-    (
-      (
-        www = new http.Directory('www/public')
-      ) => ($=>$
-        .replaceMessage(
-          req => www.serve(req)
-        )
-      )
-    )()
-  )
+var response = (status, obj) => new Message(
+  {
+    status,
+    headers: { 'content-type': 'application/json' }
+  },
+  status === 204 ? undefined : JSON.encode(obj || { status })
 )
+
+var api = new algo.URLRouter({
+  '/api/todos': function (req) {
+    switch (req.head.method) {
+      case 'GET': return response(200, db.listTodos())
+      case 'POST': return response(201, db.insertTodo(req.body.toString()))
+      default: return response(405)
+    }
+  },
+  '/api/todos/*': function (req) {
+    var id = req.head.path.substring(11) | 0
+    switch (req.head.method) {
+      case 'GET': return response(200, db.getTodo(id))
+      case 'PATCH': return response(200, db.updateTodo(id, req.body.toString()))
+      case 'DELETE': return response(204, db.deleteTodo(id))
+      default: return response(405)
+    }
+  },
+  '/api/check-todo/*': function (req) {
+    return response(200, db.checkTodo(
+      req.head.path.substring(16) | 0,
+      req.body.toString() === 'true',
+    ))
+  },
+  '/*': () => response(404)
+})
