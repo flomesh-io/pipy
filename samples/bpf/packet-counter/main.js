5b2a19cacc5ea97efe9a543c83ff052d01aa5436
pajama-coder <pajamacoder@flomesh.io>
2024-03-03 10:36:38 +0800

    [sample] Revamped samples/bpf/packet-counter in the new style of pjs

--- samples/bpf/packet-counter/main.js
+++ samples/bpf/packet-counter/main.js
@@ -1,47 +1,35 @@
-((
-  obj = bpf.object(
-    os.readFile('../../../bin/packet-counter.o')
-  ),
+var obj = bpf.object(
+  os.readFile('../../../bin/packet-counter.o')
+)
 
-  prog = obj.programs[0].load(6, 'GPL'), // BPF_PROG_TYPE_XDP
+var prog = obj.programs[0].load(6, 'GPL') // BPF_PROG_TYPE_XDP
+var map = obj.maps[0]
 
-  map = obj.maps[0],
+var PINNING_PATHNAME = '/sys/fs/bpf/packet-counter'
 
-  PINNING_PATHNAME = '/sys/fs/bpf/packet-counter',
-
-  main = (ctx) => (
-    bpf.pin(PINNING_PATHNAME, prog.fd),
-    pipy.exec(`ip link set dev lo xdpgeneric pinned ${PINNING_PATHNAME}`),
-    pipy(ctx)
-  ),
-
-  cleanup = () => (
-    pipy.exec('ip link set dev lo xdpgeneric off'),
-    pipy.exec(`rm ${PINNING_PATHNAME}`)
-  ),
-
-  dumpStats = () => void (
-    console.log('Packet stats:'),
-    map.entries().forEach(
-      ([k, v]) => (
-        console.log(' ', k.ip.u8.join('.'), v.i)
-      )
-    )
-  ),
-
-) => main()
-
-.listen(8080)
-  .serveHTTP(new Message('hello'))
-
-.task(new Data)
-  .produce(
-    () => new Timeout(1).wait().then(dumpStats)
+pipy.listen(8080, $=>$
+  .serveHTTP(
+    new Message('hello!\n')
   )
+)
 
-.exit(() => (
-  cleanup(),
-  new StreamEnd
-))
+bpf.pin(PINNING_PATHNAME, prog.fd),
+pipy.exec(`ip link set dev lo xdpgeneric pinned ${PINNING_PATHNAME}`)
+dumpStats()
 
-)()
+function dumpStats() {
+  println('Packet stats:')
+  map.entries().forEach(
+    ([k, v]) => (
+      println(' ', k.ip.u8.join('.'), v.i)
+    )
+  )
+  new Timeout(1).wait().then(dumpStats)
+}
+
+pipy.exit(
+  function () {
+    pipy.exec('ip link set dev lo xdpgeneric off')
+    pipy.exec(`rm ${PINNING_PATHNAME}`)
+  }
+)
